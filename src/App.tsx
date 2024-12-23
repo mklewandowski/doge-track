6f1b2aa687bf34810852f28d91871fa1a106a55b
mklewandowski <matt.kain.lewandowski@gmail.com>
2024-12-23 10:23:54 -0600

    feat(layout): add MUI and basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+
+import {
+  Card,
+  CardContent,
+  Container,
+  Typography,
+} from "@mui/material";
+import Stack from "@mui/material/Stack";
+import Toolbar from "@mui/material/Toolbar";
+
+import dogeLogo from './assets/doge.png'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Toolbar>
+        <img src={dogeLogo} />
+        <Typography variant={"h3"} component={"div"} m={1}>
+          Doge Track
+        </Typography>
+      </Toolbar>
+      <Container>
+      <Stack spacing={{ xs: 1, sm: 2, md: 4 }} margin={5}>
+        <Card elevation={2} sx={{ outline: "1px solid black", backgroundColor: "#424242", color: "white" }}>
+          <CardContent>
+            <Typography variant={"body1"} component={"div"} m={1}>
+              Doge Track
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card elevation={2} sx={{ outline: "1px solid black", backgroundColor: "#424242", color: "white" }}>
+          <CardContent>
+            <Typography variant={"body1"} component={"div"} m={1}>
+              Doge Track
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card elevation={2} sx={{ outline: "1px solid black", backgroundColor: "#424242", color: "white" }}>
+          <CardContent>
+            <Typography variant={"body1"} component={"div"} m={1}>
+              Doge Track
+            </Typography>
+          </CardContent>
+        </Card>
+      </Stack>
+      </Container>
+    </div>
   )
 }
 
