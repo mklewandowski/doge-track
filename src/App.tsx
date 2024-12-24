fd6c388f6a5c6891a03dc1c30dd8a99ec92dbfae
mklewandowski <matt.kain.lewandowski@gmail.com>
2024-12-23 20:41:17 -0600

    feat(layout): improve layout and text

--- src/App.tsx
+++ src/App.tsx
@@ -72,16 +72,16 @@ function App() {
         <Card elevation={2} sx={{ outline: "1px solid black", backgroundColor: "white", color: "black", width: "100%" }}>
           <CardContent sx={{ width: "100%", padding: "12px 8px 0px 8px"}}>
             <Box sx={{display: "flex", flexDirection: "row", alignItems: "flex-start", backgroundColor: "beige", padding: "8px"}}>
-              <img src={waffles} width="50%" style={{border: "3px solid black", borderRadius: "6px", transform: "rotate(1deg)", maxWidth: "200px"}} />
+              <img src={waffles} width="50%" style={{border: "3px solid black", borderRadius: "6px", transform: "rotate(2deg)", maxWidth: "200px"}} />
             </Box>
-            <Box sx={{display: "flex", flexDirection: "row", alignItems: "flex-end", marginTop: "10px"}}>
+            <Box sx={{display: "flex", flexDirection: "row", alignItems: "center", marginTop: "10px"}}>
               <RedeemIcon sx={{fontSize: "50px"}} />
               <Box sx={{display: "flex", flexDirection: "column", alignItems: "flex-start", margin: "0 6px 0 6px"}}>
-                <Typography variant={"body1"} component={"div"} sx={{margin: "2px", textAlign: "left", fontWeight: "800", lineHeight: "1"}}>
+                <Typography variant={"body1"} component={"div"} sx={{margin: "2px 2px 6px 2px", textAlign: "left", fontWeight: "800", lineHeight: "1"}}>
                   Merry Christmas, Josie!
                 </Typography>
                 <Typography variant={"body2"} component={"div"} sx={{margin: "2px", textAlign: "left", lineHeight: "1"}}>
-                  You are the proud(ish) owner of 500 Dogecoins!
+                  {`You are the proud(ish) owner of ${TOTAL_COINS} Dogecoins!`}
                 </Typography>
               </Box>
             </Box>
@@ -91,16 +91,16 @@ function App() {
         <Card elevation={2} sx={{ outline: "1px solid black", backgroundColor: "white", color: "black", width: "100%" }}>
           <CardContent sx={{ width: "100%", padding: "12px 8px 0px 8px"}}>
             <Box sx={{display: "flex", flexDirection: "row", alignItems: "flex-start", backgroundColor: "beige", padding: "8px"}}>
-              <img src={waffles2} width="50%" style={{border: "3px solid black", borderRadius: "6px", transform: "rotate(1deg)", maxWidth: "200px"}} />
+              <img src={waffles2} width="50%" style={{border: "3px solid black", borderRadius: "6px", transform: "rotate(-2deg)", maxWidth: "200px"}} />
             </Box>
-            <Box sx={{display: "flex", flexDirection: "row", alignItems: "flex-end", marginTop: "10px"}}>
+            <Box sx={{display: "flex", flexDirection: "row", alignItems: "center", marginTop: "10px"}}>
               <PsychologyAltIcon sx={{fontSize: "50px"}} />
               <Box sx={{display: "flex", flexDirection: "column", alignItems: "flex-start", margin: "0 6px 0 6px"}}>
-                <Typography variant={"body1"} component={"div"} sx={{margin: "2px", textAlign: "left", fontWeight: "800", lineHeight: "1"}}>
-                  What is Dodgecoin?
+                <Typography variant={"body1"} component={"div"} sx={{margin: "2px 2px 6px 2px", textAlign: "left", fontWeight: "800", lineHeight: "1"}}>
+                  What is Dogecoin?
                 </Typography>
                 <Typography variant={"body2"} component={"div"} sx={{margin: "2px", textAlign: "left", lineHeight: "1"}}>
-                  A cryptocurrency. Like Bitcoin Only based on a dog meme and slightly dumber.
+                  A cryptocurrency like Bitcoin... only based on a dog meme and slightly dumber.
                 </Typography>
               </Box>
             </Box>
@@ -111,13 +111,13 @@ function App() {
         <Card elevation={2} sx={{ outline: "1px solid black", backgroundColor: "white", color: "black", width: "100%" }}>
           <CardContent sx={{ width: "100%", padding: "12px 8px 0px 8px"}}>
             <Box sx={{display: "flex", flexDirection: "row", alignItems: "flex-start", backgroundColor: "beige", padding: "8px"}}>
-              <img src={waffles3} width="50%" style={{border: "3px solid black", borderRadius: "6px", transform: "rotate(1deg)", maxWidth: "200px"}} />
+              <img src={waffles3} width="50%" style={{border: "3px solid black", borderRadius: "6px", transform: "rotate(2deg)", maxWidth: "200px"}} />
             </Box>
-            <Box sx={{display: "flex", flexDirection: "row", alignItems: "flex-end", marginTop: "10px"}}>
+            <Box sx={{display: "flex", flexDirection: "row", alignItems: "center", marginTop: "10px"}}>
               <LocalAtmIcon sx={{fontSize: "50px"}} />
               <Box sx={{display: "flex", flexDirection: "column", alignItems: "flex-start", margin: "0 6px 0 6px"}}>
-                <Typography variant={"body1"} component={"div"} sx={{margin: "2px", textAlign: "left", fontWeight: "800", lineHeight: "1"}}>
-                  What is it worth?
+                <Typography variant={"body1"} component={"div"} sx={{margin: "2px 2px 6px 2px", textAlign: "left", fontWeight: "800", lineHeight: "1"}}>
+                  Will I get rich?
                 </Typography>
                 <Typography variant={"body2"} component={"div"} sx={{margin: "2px", textAlign: "left", lineHeight: "1"}}>
                   No idea! I'm a dog! But you can see your current Dogecoin value below.
@@ -129,7 +129,12 @@ function App() {
 
         <Card elevation={2} sx={{ outline: "1px solid black", backgroundColor: "white", color: "black", width: "100%" }}>
           <CardContent sx={{ width: "100%", padding: "12px 8px 4px 8px"}}>
-            <Typography variant={"body1"} component={"div"} m={1}>YOUR Dogecoins are worth</Typography>
+            <Box margin={1} sx={{display: "flex", flexDirection: "row", alignItems: "flex-end"}}>
+              <img src={dogeLogo} style={{maxWidth: "50px"}} />
+              <Typography variant={"body1"} component={"div"} m={1} sx={{fontWeight: "400"}}>
+                YOUR Dogecoins are worth
+              </Typography>
+            </Box>
             <Typography variant={"h3"} component={"h3"} m={1} sx={{fontWeight: "800", backgroundColor: "yellowgreen", padding: "20px 0 20px 0", borderRadius: "8px"}}>
               ${dogeTotalValue.toFixed(2)}
             </Typography>
@@ -141,13 +146,13 @@ function App() {
         <Card elevation={2} sx={{ outline: "1px solid black", backgroundColor: "white", color: "black", width: "100%" }}>
           <CardContent sx={{ width: "100%", padding: "12px 8px 0px 8px"}}>
             <Box sx={{display: "flex", flexDirection: "row", alignItems: "flex-start", backgroundColor: "beige", padding: "8px"}}>
-              <img src={waffles4} width="50%" style={{border: "3px solid black", borderRadius: "6px", transform: "rotate(1deg)", maxWidth: "200px"}} />
+              <img src={waffles4} width="50%" style={{border: "3px solid black", borderRadius: "6px", transform: "rotate(-2deg)", maxWidth: "200px"}} />
             </Box>
-            <Box sx={{display: "flex", flexDirection: "row", alignItems: "flex-end", marginTop: "10px"}}>
-              <CurrencyExchangeIcon sx={{fontSize: "50px"}} />
+            <Box sx={{display: "flex", flexDirection: "row", alignItems: "center", marginTop: "10px"}}>
+              <CurrencyExchangeIcon sx={{fontSize: "40px"}} />
               <Box sx={{display: "flex", flexDirection: "column", alignItems: "flex-start", margin: "0 6px 0 6px"}}>
-                <Typography variant={"body1"} component={"div"} sx={{margin: "2px", textAlign: "left", fontWeight: "800", lineHeight: "1"}}>
-                  How do I cash it in?
+                <Typography variant={"body1"} component={"div"} sx={{margin: "2px 2px 6px 2px", textAlign: "left", fontWeight: "800", lineHeight: "1"}}>
+                  How do I cash in my coins?
                 </Typography>
                 <Typography variant={"body2"} component={"div"} sx={{margin: "2px", textAlign: "left", lineHeight: "1"}}>
                   To get real money, you need to talk to the Big Cookie. I don't even have a bank account.
